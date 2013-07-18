82cc59191bbfaffb1582ad6fb86a95e4d45a126a
JohnnyAppleDev <honaker215@gmail.com>
2013-07-17 20:05:40 -0400

    Wrote multiplier in assembly.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,46 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+	@R0
+	D=M
+	@MULTBYZERO
+	D;JEQ
+	@R1
+	D=M
+	@MULTBYZERO
+	D;JEQ
+	@i
+	M=1
+	@ans
+	M=0
+
+(LOOP)
+	@R0
+	D=M
+	@ans
+	M=M+D	// ans = ans + *R0
+	D=M
+	@R2
+	M=D
+	@i		// D=i
+	D=M
+	@R1		// A = R1
+	D=D-M	// D= i - *R1
+	@END
+	D;JGE 	// If (i-R1) == 0 goto END
+	@i
+	M=M+1	// i++
+	@LOOP
+	0;JMP
+
+	
+(END)
+	@END
+	0;JMP
+
+(MULTBYZERO)
+	@R2
+	M=0
+	@END
+	0;JMP
+
