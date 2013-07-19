8d1aac935d048f0530068a881c9b40168f108c55
JohnnyAppleDev <honaker215@gmail.com>
2013-07-19 16:46:10 -0400

    Blocked out basic SymbolTable.

---
+++ projects/06/symboltable.py
@@ -0,0 +1,13 @@
+class SymbolTable:
+
+	def __init__(self):
+		pass
+
+	def addEntry(self, symbol, address):
+		pass
+
+	def contains(self, symbol):
+		pass
+
+	def getAddress(self, symbol):
+		pass
