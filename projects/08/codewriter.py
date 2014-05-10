381881860e658623c0ed30d01fc0ebaa8f2eb323
JHonaker <honaker215@gmail.com>
2014-05-09 22:10:47 -0400

    Move over parser/code gen files from chapter 7.

---
+++ projects/08/codewriter.py
@@ -0,0 +1,242 @@
+# Current temporary stage one implementation of the VM Code Generator
+#
+# Stack Arithmetic, Logical Commands, push contstant x
+# 
+# Reference Note: -1 is true, 0 is false
+
+import os
+from parser import *
+
+class CodeWriter:
+
+    def __init__(self, outfile):
+        """Opens the file output file/stream and gets
+        ready to write into it."""
+
+        isDirectory = outfile[-3:] != '.vm'
+
+        self.outfile = open(outfile[:-3] + '.asm', 'w')
+        
+        # Create the current label.
+        # This is machine read and wrote, so numeric labels will suffice
+        self.nextLabel = 0
+
+        # Static label, this changes with each filename:
+        # Forms the command labels @Xxx.j
+        self.staticPrefix = ''
+
+        if (isDirectory):
+            for file in os.listdir('/' + outfile):
+                if file.endswith(".vm"):
+                    self.setFileName('./' + outfile + '/' + file)
+
+        else:
+            self.setFileName(outfile)
+
+        self.outfile.close()
+        print 'Closed file: ' + outfile[:-3] + '.asm'
+
+
+    def setFileName(self, fileName):
+        """Informs the code writer that the translations of
+        a new VM file has started."""
+        p = parser(fileName)
+        self.staticPrefix = fileName.split('.')[0]
+
+        while(p.hasMoreCommands()):
+            p.advance()
+            self.dispatchWriter(p.commandType(), p.currentCommand)
+
+    # Label creating functions
+    def newLabel(self):
+        """Creates the next unused label."""
+        self.nextLabel += 1
+        return 'L' + str(self.nextLabel)
+    
+    def staticLabel(self, index):
+        """Returns the static label of the index."""
+        return self.staticPrefix + '.' + str(index)
+
+    
+    # Command writers and the dispatcher
+    def dispatchWriter(self, commandType, command):
+        """Dispatches the write for the given command type."""
+
+        if (commandType == 'C_ARITHMETIC'):
+            self.writeArithmetic(command[0])
+        elif (commandType == 'C_POP' or commandType == 'C_PUSH'):
+            self.writePushPop(command[0], command[1], command[2])
+        else:
+            pass
+
+    def writeArithmetic(self, command):
+        """Writes the assembly code that is the given translation
+        of the command given."""
+        print command
+        # Arithmetic
+        if (command == 'add'):
+            self.binaryOp('D+A')
+        elif (command == 'sub'):
+            self.binaryOp('A-D')
+        elif (command == 'neg'):
+            self.unaryOp('-D')
+        # Bitwise
+        elif (command == 'and'):
+            self.binaryOp('D&A')
+        elif (command == 'or'):
+            self.binaryOp('D|A')
+        elif (command == 'not'):
+            self.unaryOp('!D')
+        # Logical
+        elif (command == 'eq'):
+            self.logicalOp('JEQ')
+        elif (command == 'gt'):
+            self.logicalOp('JGT')
+        elif (command == 'lt'):
+            self.logicalOp('JLT')
+
+    def writePushPop(self, command, segment, index):
+        """Writes the assembly code that is the translation of the
+        given command, where command is either C_PUSH or C_POP."""
+        if (command == 'push'):
+
+            if (segment == 'constant'):
+                self.constToStack(index)
+            else:
+                self.regToStack(segment, index)
+
+            self.increaseStackPointer()
+
+        elif (command == 'pop'):
+
+            self.decreaseStackPointer()
+            self.stackToReg(segment, index)
+
+
+    # Stack Operator Commands
+    def unaryOp(self, comp):
+        """Pop one argument off the stack, perform comp.
+        Properly manages stack pointer."""
+        self.decreaseStackPointer()
+        self.stackToDest('D')
+        self.cCommand('D', comp)
+        self.compToStack('D')
+        self.increaseStackPointer()
+
+    def binaryOp(self, comp):
+        """Pops two arguments off the stack, perform comp.
+        Properly manages stack pointer."""
+        self.decreaseStackPointer()
+        self.stackToDest('D')
+        self.decreaseStackPointer()
+        self.stackToDest('A')
+        self.cCommand('D', comp)
+        self.compToStack('D')
+        self.increaseStackPointer()
+
+    def logicalOp(self, jump):
+        """Pops two arguments off the stack, perform comp.
+        Properly manages stack pointer.
+        Returns -1 for true, 0 for false."""
+        # Create new label for branching
+        # Note, this does not write it to asm
+        trueLabel = self.newLabel()
+
+        self.decreaseStackPointer()
+        self.stackToDest('D')
+        self.decreaseStackPointer()
+        self.stackToDest('A')
+        self.cCommand('D', 'A-D')
+        self.compToStack('-1')
+        self.aCommand(trueLabel)
+        self.cCommand(None, 'D', jump)
+        self.compToStack('0')
+        self.lCommand(trueLabel)
+        self.increaseStackPointer()
+
+
+    # Register manipulators
+    def registerAddress(self, segment, index):
+        """Read the address of the register (segment index) into A and D."""
+        segments = {'pointer': 3, 'temp': 5, 'local': 'LCL', 'argument': 'ARG',
+                'this': 'THIS', 'that': 'THAT', 'static': self.staticLabel(index) }
+
+        self.aCommand(index)
+        self.cCommand('D', 'A')
+        self.aCommand(segments[segment])
+        if (segment in ['local', 'argument', 'this', 'that']):
+            self.cCommand('A', 'M')
+        self.cCommand('AD', 'D+A')
+
+
+    # Write to stack commands
+    def constToStack(self, const):
+        """Place value at address to stack."""
+        self.aCommand(const)
+        self.cCommand('D', 'A')
+        self.compToStack('D')
+
+    def compToStack(self, comp):
+        """Place the computation into the top of stack."""
+        self.loadSP()
+        self.cCommand('M', comp)
+
+    def regToStack(self, segment, index):
+        """Place the value of the register on the stack."""
+        self.registerAddress(segment, index)
+        self.cCommand('D', 'M') # compToStack overwrites A, so move to D first
+        self.compToStack('D')
+
+
+    # Read from stack commands
+    def stackToDest(self, dest):
+        """Place the value in the stack in the dest."""
+        self.loadSP()
+        self.cCommand(dest, 'M')
+
+    def stackToReg(self, segment, index):
+        """Place the value in the stack into the appropriate register."""
+        self.registerAddress(segment, index)
+        self.aCommand('R13')
+        self.cCommand('M', 'D')
+        self.stackToDest('D')
+        self.aCommand('R13')
+        self.cCommand('A', 'M')
+        self.cCommand('M', 'D')
+
+
+    # Stack pointer manipulators
+    def increaseStackPointer(self):
+        """Increase the stack pointer."""
+        self.aCommand('SP')
+        self.cCommand('M', 'M+1')
+
+    def decreaseStackPointer(self):
+        """Decrease the stack pointer."""
+        self.aCommand('SP')
+        self.cCommand('M', 'M-1')
+
+    def loadSP(self):
+        self.aCommand('SP')
+        self.cCommand('A', 'M')
+
+
+    # Command Writers
+    def cCommand(self, dest, comp, jump = None):
+        """Write out a C command in Hack assembly."""
+        if (dest != None):
+            self.outfile.write(dest + '=')
+        self.outfile.write(comp)
+        if (jump != None):
+            self.outfile.write(';' + jump)
+        self.outfile.write('\n')
+
+    def aCommand(self, addr):
+        """Write out an A command in Hack assemply."""
+        self.outfile.write('@' + str(addr) + '\n')
+
+    def lCommand(self, label):
+        """Creates a new (label) for the assembler."""
+        self.outfile.write('('+label+')\n')
+
+
---
+++ projects/08/parser.py
@@ -0,0 +1,95 @@
+# Current temporary stage one implementation of the VM parser
+#
+# Stack Arithmetic, Logical Commands, push contstant x
+
+class parser:
+
+    def __init__(self, infile):
+        """Opens the input file/stream and gets ready to parse it."""
+
+        # Open the file and read the raw commands
+        f = open(infile)
+        self.commands = f.readlines()
+        f.close()
+
+        # Strip comments and whitespace from the end of lines
+        self.commands = [com.split('//')[0] for com in self.commands]
+        self.commands = [com.strip() for com in self.commands]
+
+        # Remove blank lines and comment-only lines from the command list
+        self.commands = self._trimLines()
+
+        # Initialize the first command to -1 (Initially blank command)
+        self.cmdIndex = -1
+        self.currentCommand = None
+
+    def _trimLines(self):
+        """Removes blank lines and comment (//) only lines from self.commands"""
+
+        tempCommands = []
+        for i in range(0, len(self.commands)):
+            if self.commands[i] != '' and self.commands[i][0:2] != '//':
+                tempCommands.append(self.commands[i])
+        return tempCommands
+
+
+    def hasMoreCommands(self):
+        """Are there more commands in the input?"""
+
+        return self.cmdIndex < len(self.commands) - 1
+
+    def advance(self):
+        """Reads the next command from the input and makes it
+        the current command. Should be called only if hasMoreCommands(self)
+        is true. Initially there is no current command"""
+
+        if (self.hasMoreCommands() == True):
+            self.cmdIndex += 1
+            self.currentCommand = self.commands[self.cmdIndex].split(' ')
+
+    def commandType(self):
+        """Returns the type of the current VM command.
+        C_ARITHMETIC is returned for all the artihmetic commands."""
+
+        C_ARITHMETIC = [
+                'add', 'sub', 'neg', 'eq', 'gt',
+                'lt', 'and', 'or', 'not'
+                ]
+
+        command = self.currentCommand
+
+
+        if (command[0] in C_ARITHMETIC):
+            return 'C_ARITHMETIC'
+
+        if (command[0] == 'push'):
+            return 'C_PUSH'
+
+        if (command[0] == 'pop'):
+            return 'C_POP'
+
+
+    def arg1(self):
+        """Returns the first argument of the current command.
+        In the case of C_ARITHMETIC the command itself (add,
+        sub, etc.) is returned. Should not be called if the
+        current command is C_RETURN."""
+
+        if (self.commandType() == 'C_RETURN'):
+            return None
+
+        elif (self.commandType() == 'C_ARITHMETIC'):
+            return self.currentCommand[0]
+
+        else:
+            return self.currentCommand[1]
+
+    def arg2(self):
+        """Returns the second argument of the current command.
+        Should be called only if the current command is
+        C_PUSH, C_POP, C_FUNCTION, OR C_CALL."""
+
+        if (self.commandType() in ['C_PUSH', 'C_POP', 'C_FUNCTION', 'C_CALL']):
+            return self.currentCommand[2]
+        else:
+            return None
